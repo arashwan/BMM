6d0d630e9970f3e4abe37e878d581cf3cab85c14
Abdullah Rashwan <abdullahrashwan@v1020-wn-103-23.campus-dynamic.uwaterloo.ca>
2017-02-02 14:03:48 -0500

    Initial Commit

---
+++ BMM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BMM
+//
+//  Created by Abdullah Rashwan on 2017-02-02.
+//  Copyright © 2017 Abdullah Rashwan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
